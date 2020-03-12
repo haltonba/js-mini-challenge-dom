88d0f91908934c0bb4344271ff6319f06317ca67
haltonba <haltonba94@gmail.com>
2020-03-12 17:38:46 -0400

    Yo

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.getElementsByClassName("player-container")[0];
+
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+const header = document.getElementById("header")
+console.log(`Here's your header: ${header.innerText}`)
 
 
 /***** Deliverable 2 *****/
 
-
+header.style.color = "red"
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
 
 
+PLAYERS.forEach(function (player) {
+    let playerDiv = document.createElement("div");
+    playerDiv.innerHTML = `
+        <div class="player" data-number="${player.number}">
+            <h3>${player.name} (<em>${player.nickname}</em>)</h3>
+            <img src="${player.photo}" alt="${player.name}">
+        </div>
+    `
+    container.appendChild(playerDiv)
+})
 /***** Deliverable 4 *****/
+
+})
